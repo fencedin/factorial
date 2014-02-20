d52f47941fb783f606c3f919123f1dc457bd1c7a
epicodus <student@epicodus.com>
2014-02-20 11:21:52 -0800

    finished code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
-
-
-
-
+var factorialize = function(integer) {
+var storageArray = [];
+  if (integer === 0 || integer === 1) {
+    return 1;
+  } 
+  if (integer > 1) {
+    for (var i = 0; i < integer; i++) {
+      storageArray += (integer-i)
+    }
+  storageArray = storageArray.split('').map(Number)
+  storageArray = storageArray.reduce(function(x, y) {return x*y } );
   
+  };  
+  return storageArray
+};
 
+$(document).ready(function() {
+  $("form#factorial-number").submit(function(event) {
+    var factNumber = $("input#factNumber").val();
+    var result = factorialize(factNumber);
 
+    $(".result").text(result);
+    
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,25 @@
-describe('', function() {
-  it('', function() {
-    titleCase('').should.equal('');
+describe('factorialize', function() {
+  it('it returns the factorial for the number zero', function() {
+    factorialize(0).should.equal(1);
+  });
+
+  it('it returns the factorial for the number one', function() {
+    factorialize(1).should.equal(1);
+  });
+  
+  it('it returns the factorial for any number', function() {
+    factorialize(2).should.equal(2);
+  });
+  
+  it('it returns the factorial for any number', function() {
+    factorialize(3).should.equal(6);
+  });
+  
+  it('it returns the factorial for any number', function() {
+    factorialize(5).should.equal(120);
+  });
+  
+  it('still works with big numbers(up to 9)', function() {
+    factorialize(9).should.equal(362880);
   });
 });
